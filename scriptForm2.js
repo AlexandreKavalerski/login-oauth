432454ae7a33ff3060d571023574d2c31364d624
Alexandre <kavalerskialexandre@gmail.com>
2017-12-16 02:13:11 -0300

    Initial Commit

---
+++ scriptForm2.js
@@ -0,0 +1,31 @@
+function clickSubmit(){
+    console.log("okok");
+	var chave = getParameterByName('chaveApp');
+    document.getElementById("chaveApp").value = chave;
+    var login = $('#login').val();
+    var senha = $('#senha').val();
+    console.log('login: ' + login);
+    console.log('senha: ' + senha);
+    console.log('chave: ' + chave);
+    $.post('http://localhost:3000/auth', {login: login, senha: senha, chaveApp: chave})
+        .done(function(result){
+            if(result.sucesso){
+                console.log(result);
+                console.log(result.urlCallback + '?access_token=' + result.token);
+                // window.location.replace(result.urlCallback + '?access_token=' + result.token);
+            }
+        });
+	// document.formLogin.target = "_self";
+	// document.formLogin.action = "http://localhost:3000/auth";
+	// document.formLogin.submit();
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
